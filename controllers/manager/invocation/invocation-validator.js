e99ad94e40cd2596eae6be9f615c2d311a059330
doanhv2606 <svmc.sel16f@gmail.com>
2023-12-05 15:24:47 +0700

    feedbck v11

--- controllers/manager/invocation/invocation-validator.js
+++ controllers/manager/invocation/invocation-validator.js
@@ -1,40 +1,38 @@
 'use strict';
 const { body } = require('express-validator');
 
-exports.invocationValidator = [
+exports.invocationValidator = (req, res, next) => {
     body('invocation')
         .exists().withMessage('invocation is required')
-        .isObject().withMessage('invocation must be an object'),
+        .isObject().withMessage('invocation must be an object');
 
     body('invocation.inviteeId')
-        .exists().withMessage('name is required'),
+        .exists().withMessage('name is required');
 
     body('invocation.name')
-        .exists().withMessage('Name is required'),
+        .exists().withMessage('Name is required');
 
     body('invocation.phone')
-        .exists().withMessage('Phone is required'),
+        .exists().withMessage('Phone is required');
 
     body('invocation.status')
         .exists().withMessage('Status is required')
         .isNumeric().withMessage('Status must be a number')
         .custom((value, { req }) => {
-            // If status is Tham gia (1)
-            if (value == 1) {
-                // Make vehicles required and location optional
-                req.body.invocation.vehicles = req.body.invocation.vehicles || ''; // You can customize this based on the type of 'vehicles'
+            const status = parseInt(value);
 
+            if (status === 1) {
+                // If status is Tham gia (1)
+                // Make vehicles required and location optional
                 body('invocation.vehicles')
                     .exists().withMessage('Vehicles is required');
 
                 body('invocation.location')
                     .optional()
                     .isNumeric().withMessage('Location must be a number');
-            } else if (value == -1) {
+            } else if (status === -1) {
                 // If status is Không tham gia (-1)
                 // Make both vehicles and location optional
-                req.body.invocation.vehicles = req.body.invocation.vehicles || ''; // You can customize this based on the type of 'vehicles'
-
                 body('invocation.vehicles')
                     .optional();
 
@@ -42,8 +40,6 @@ exports.invocationValidator = [
                     .optional();
             } else {
                 // If the status is neither 1 nor -1, both vehicles and location are optional
-                req.body.invocation.vehicles = req.body.invocation.vehicles || ''; // You can customize this based on the type of 'vehicles'
-
                 body('invocation.vehicles')
                     .optional();
 
@@ -52,6 +48,13 @@ exports.invocationValidator = [
             }
 
             return true;
-        }),
+        });
 
-];
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    next();
+};
+
